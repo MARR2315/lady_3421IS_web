290ed04ffe33dc8d853156404d140f3b9a2d7a97
MARR2315 <martha1320114048@uptecamac.edu.mx>
2021-09-14 10:09:13 -0500

    js

---
+++ debuggear/main.js
@@ -0,0 +1,63 @@
+/*var n1 =10;
+var n2 =20;
+var test =0;
+
+if(n1<n2){
+    console.log("n1 es menor que n2");
+}else{
+    console.log("n2 es mayor que n1");
+   test =suma;
+}
+n1 =100;
+console.log(n1);
+console.log(test);
+
+function suma(){
+  let resultado = n1 +n2;
+  return resultado;
+}
+
+var elementos =[];
+var elementos2 = Array();
+var elementos3 = ["hola",1,8.5,true,5,7,8,9,10];
+var elementos4 = [];
+
+var palabra= "hola mundo";
+var contenedor =[];
+
+
+for (let i = 0; i < elementos3.length; i++) {
+    console.log[elementos3[i]];
+    elementos4.push(elementos3[i]);
+    
+}
+console.log(elementos4);
+
+for (let i = 0; palabra.length; i++) {
+   contenedor.push(palabra[i]);
+}
+console.log(contenedor);*/
+
+var martha = {
+    "nombre" :"MARTHA",
+    "apellido" : "RIVERO",
+    "edad" : 19,
+    "gustos":{
+      "musica" :{
+        "rock pop" : "no confies en mi",
+        "pop en ingles" : "one and only",
+        "pop" : "rompo contigo",
+        "banda" : "en tu perra vida",
+        "hip hop" :{
+          "cancion1" : "anda y ve",
+          "cancion2" : "lo dudo",
+          "cancion3" : "mi gusto es"
+        },
+      },
+      "baile" : "suffle",
+      "canto" :  "pop",
+      "deporte" : "box",
+      "lenguaje" : "c#"
+    }
+  }
+  console.log(martha);
